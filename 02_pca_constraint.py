777d378e78745fe6eb2f7102c7d631b52a2726ea
nir <nir@streem.ai>
2020-11-21 17:34:56 +0100

    update pca constraint

--- 02_pca_constraint.py
+++ 02_pca_constraint.py
@@ -1,5 +1,9 @@
 # Create PCA encoder with orientation constraint
 # Reference https://github.com/ageron/handson-ml
+
+#IPython.core.debugger.Pdb.set_trace()
+
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
@@ -21,13 +25,18 @@ assert tf.version.VERSION == '2.3.1'
 assert tf.executing_eagerly() == True
 
 # ---------- params -------------------------------------------
-output_folder = 'outputs/'
-
-ITERATIONS = 10
+output_folder = 'output/'
 # -------------------------------------------------------------
-
+now = datetime.now()
+file_header = output_folder + now.strftime("%Y%m%d_%H%M%S_")
 
 X = get_data()
+
+from sklearn.decomposition import PCA
+X_pca = PCA(n_components=2).fit_transform(X)
+
+loss_tracker = keras.metrics.Mean(name="loss")
+loss_shift_tracker = keras.metrics.Mean(name="loss")
 
 
 class my_model(keras.Model):
@@ -37,6 +46,9 @@ class my_model(keras.Model):
         self.hidden_decode = keras.layers.Dense(3)
         self.X_align = X_align
         self.y_align = y_align
+
+    #         self.metrics.append(keras.metrics.Mean(name="base_loss"))
+    #         self.metrics.append(keras.metrics.Mean(name="shift_loss"))
 
     def call(self, inputs):
         encoded = self.hidden_encode(inputs)
@@ -57,21 +69,21 @@ class my_model(keras.Model):
     # https://www.tensorflow.org/guide/eager
 
     def train_step(self, data):
-        # Unpack the data. Its structure depends on your model and
-        # on what you pass to `fit()`.
         x, y = data
 
         # from IPython.core.debugger import Tracer; Tracer()()
-        # IPython.core.debugger.Pdb.set_trace()
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)  # Forward pass
             y_align_pred = self.latent_dim(np.array([X_align, X_align]))
             # Compute the loss value
             # (the loss function is configured in `compile()`)
-            # loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
-            loss = tf.reduce_sum(tf.square(y - y_pred)) + 1 / 100 * tf.reduce_sum(
-                tf.square(y_align_pred - np.array([y_align, y_align])))
+            shift_loss = tf.reduce_sum(tf.square(y_align_pred - np.array([y_align, y_align])))
+            base_loss = tf.reduce_sum(tf.square(y - y_pred))
+            loss = base_loss + 1 / x.shape[0] * shift_loss
+            # l = tf.reduce_sum(tf.square(y_align_pred - np.array([y_align,y_align])))
+
+        #             loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses) + l
 
         # Compute gradients
         trainable_vars = self.trainable_variables
@@ -79,35 +91,71 @@ class my_model(keras.Model):
         gradients = tape.gradient(loss, trainable_vars)
         # Update weights
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        loss_tracker.update_state(base_loss)
+        loss_shift_tracker.update_state(shift_loss)
         # Update metrics (includes the metric that tracks the loss)
         self.compiled_metrics.update_state(y, y_pred)
         # Return a dict mapping metric names to current value
-        return {m.name: m.result() for m in self.metrics}
+        dict_res = {m.name: m.result() for m in self.metrics}
+        dict_res.update({"base_loss": loss_tracker.result(), "shift_loss": loss_shift_tracker.result()})
+        return dict_res
 
 
-X_align = [1, 1, 1]
-y_align = [-0.37301758, 1.1074662]
-
-model = my_model(units=3, X_align=X_align, y_align=y_align)
-model.compile(loss="mse")
-
-history = model.fit(X, X, epochs = 500, verbose = 0)#, callbacks = [tensorboar_cb])
-
-model_encoder = model.get_encoder_model()
+# X_align = [1,1,1]
+# y_align = [-0.37301758,  1.1074662]
+X_align = [0, 0, 0]
+y_align = [0, 0]
 
 
-X_align_train = np.array([X_align,X_align])
-y_align_train = np.array([y_align,y_align])
+def plot_model_res(model):
+    model_encoder = model.get_encoder_model()
+    X_align_train = np.array([X_align,X_align])
+    y_align_train = np.array([y_align,y_align])
+    model_encoder.compile(loss = "mse")
+    coding = model_encoder.predict(X)
+    align_coding_vect = model_encoder.predict(X_align_train)
+    align_coding = align_coding_vect[0]
+    plt.plot(coding[:,0], coding[:,1], '.', alpha = 0.2)
+    plt.plot(y_align[0], y_align[1], 'rx')
+    plt.plot(align_coding[0], align_coding[1], 'r.')
 
-model_encoder.compile(loss = "mse")
 
-coding = model_encoder.predict(X)
-align_coding_vect = model_encoder.predict(X_align_train)
-align_coding = align_coding_vect[0]
+MODEL_NUM = 3
+model_list = []
+# opt = keras.optimizers.Adam(learning_rate=0.01)
+opt = keras.optimizers.SGD(learning_rate=0.01)
+
+for i in range(MODEL_NUM):
+    print(f'{i} / {MODEL_NUM}       \r', end = '')
+    model = my_model(units=3, X_align=X_align, y_align=y_align)
+    model.compile(loss = 'mse', optimizer = opt)
+    history = model.fit(X, X, epochs=20, verbose=0, batch_size=10)#, callbacks = [tensorboar_cb])
+    model_list.append(model)
 
 
-# ---- plot results -----------------
+
+key_list = ['base_loss', 'shift_loss']
+
+
+def save_plot(plt, filename):
+    print(f'saving {filename}')
+    plt.savefig(filename)
+
+
 plt.figure()
-plt.plot(coding[:,0], coding[:,1],'.')
-plt.plot(y_align[0], y_align[1], 'rx')
-plt.plot(align_coding[0], align_coding[1], 'r.')
+for k in key_list:
+    plt.plot(history.history[k])
+plt.legend(key_list)
+
+save_plot(plt, file_header+'loss.png')
+
+
+plt.figure()
+
+for model in model_list:
+    plot_model_res(model)
+plt.scatter(X_pca[:,0], X_pca[:,1], s=5, c='C7',alpha = 0.2)
+
+save_plot(plt, file_header+'dim_reduction.png')
+
+
